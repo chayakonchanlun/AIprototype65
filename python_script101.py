b528f54409c3e6bf410e6651c9a428e3a345a3e1
chayakonchanlun <76931050+chayakonchanlun@users.noreply.github.com>
2023-02-13 15:46:10 +0700

    Add files via upload

---
+++ python_script101.py
@@ -0,0 +1,37 @@
+import argparse #สำหรับ รับ input จากภายนอก
+
+def parse_input():
+     parser = argparse.ArgumentParser(description='test program to learn about argparse and subprocess')
+     parser.add_argument(
+          'm', 
+          type=int,
+          help='value of M positional argument')
+     parser.add_argument(
+          '--x', 
+          type=int,
+          help='value of x')
+
+     parser.add_argument(
+         '--yval',
+         type=int,
+         default=3,
+         help='value of y')
+
+     args = parser.parse_args()
+     return args
+
+
+def print_other():
+     print('something else')
+     
+def print_one():
+     print('11111')
+
+if __name__ == "__main__":
+    x = parse_input()
+    print_other()
+    print_one()
+    print_other()
+
+    print(f"yval = {x.yval}")
+    print(f'xt = {(x.x)*2}')
