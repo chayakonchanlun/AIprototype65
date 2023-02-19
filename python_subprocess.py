4842d2334fd52ee9c11ec36ed5a231cc3d11af2f
chayakonchanlun <76931050+chayakonchanlun@users.noreply.github.com>
2023-02-20 00:51:04 +0700

    Add files via upload

--- python_subprocess.py
+++ python_subprocess.py
@@ -7,15 +7,20 @@ if __name__ == "__main__":
     
     ## use output of subprocess
     sum = 0 
-    for i in range(50):
-        if i % 2 != 0 :
+    count = 0 
+    for i in range(100):
+        if i % 2 != 0 : #หาร2แล้วไม่เท่ากับ 0
+            count +=1
 
             process1 = subprocess.Popen(["python", "python_script101.py", "--x",f'{i}'],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            out, err = process1.communicate()
+            out, err = process1.communicate()1
             print(out.decode('utf-8'))
+            
+            sum += (i*2)
+            
 
 #ผลรวม xt= xt*2
-            sum += (i*2)
-    print(f'Summation of xt = {sum}')
+        print(f'Round = {count}')        
+    print(f'SUM_XT = {sum}')
     #HW สั่งรัน python_script101.py 50 รอบ โดย x = 1,3,5,7,... 
     #แล้วให้เอา output ของ xt มารวมกัน แล้ว print ออกมา
